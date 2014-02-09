92fe76cae1f54bd48686b81fd4e4fc10a5339162
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2014-02-10 00:00:19 +0100

    lurk around

--- components.py
+++ components.py
@@ -1,7 +1,9 @@
-from ecs import models
+import pyglet
+import ecs
 
-class Renderable(Component):
+class Renderable(ecs.Component):
 	"""Component that can be render on screen"""
 	def __init__(self, sprite):
 		super(Renderable, self).__init__()
 		self.sprite = sprite
+
--- systems.py
+++ systems.py
@@ -1,14 +1,36 @@
-from ecs import models
+import ecs
+import pyglet
+from pyglet.window import key
 
 import components
 
-class Renderer(System):
-	"""Renderer"""
+class Renderer(ecs.System):
+    """Renderer"""
+    def __init__(self):
+        super(Render, self).__init__()
 
-	def __init__(self):
-		super(Render, self).__init__()
+    def pre_update(self):
+        self.window.clear()
 
-	def update(self, dt):
+    def update(self, dt):
         for entity, renderable_component in \
-        	self.entity_manager.pairs_for_type(Renderable):
+            self.entity_manager.pairs_for_type(Renderable):
                 renderable_component.sprite.draw()
+
+class Input(ecs.Component):
+    """Input handler"""
+    def __init__(self, window: pyglet.window):
+        super(Input, self).__init__()
+        # Register the event lister from the window
+        @pyglet.window.event
+        def on_key_press(symbol, modifier):
+            self.on_key_press(symbol, modifier)
+
+    def pre_update(self):
+        pass
+
+    def on_key_press(self, symbol, modifier):
+        if symbol == key.LEFT:
+            pass
+        elif symbol == key.RIGHT:            
+            pass
