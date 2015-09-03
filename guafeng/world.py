f449bfcddc478401cf13d1499b8927251f47181a
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2015-09-03 17:51:38 +0200

    Add draft for save/restore

--- guafeng/world.py
+++ guafeng/world.py
@@ -1,8 +1,11 @@
 import pyglet
 import ecs
+import pickle
+from pyglet.window import key
 
 from guafeng import archetypes
 from guafeng import systems
+from guafeng.components import InputComponent
 
 
 class World:
@@ -12,6 +15,7 @@ class World:
         self.window = pyglet.window.Window()
 
         entity_manager = ecs.EntityManager()
+        self.entity_manager = entity_manager
         system_manager = ecs.SystemManager(entity_manager)
 
         render_system = systems.RenderSystem(self)
@@ -26,6 +30,12 @@ class World:
         self.map = archetypes.create_map(entity_manager, system_manager)
         self.player = archetypes.create_player(entity_manager, system_manager)
         self.camera = archetypes.create_camera(entity_manager, system_manager)
+
+        save_load = entity_manager.create_entity()
+        input_sl = InputComponent()
+        input_sl.add_keybind(key.F5, lambda: self.save())
+        input_sl.add_keybind(key.F9, lambda: self.load())
+        entity_manager.add_component(save_load, input_sl)
 
         # Start the pyglet application
         @self.window.event
@@ -42,3 +52,11 @@ class World:
         self.window.push_handlers(*self._handlers_args, **self._handlers_kwargs)
         pyglet.app.run()
         self.window.pop_handlers()
+
+    def save(self, name='save.gf'):
+        with open(name, 'w') as fd:
+            pickle.dump(self.entity_manager, fd)
+
+    def load(self, name='save.gf'):
+        with open(name, 'rb') as fd:
+            self.entity_manager = pickle.load(fd)
